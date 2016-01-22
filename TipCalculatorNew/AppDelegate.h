f72534ca4b469166cf8bd68d4b10555bb639515b
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-22 12:32:00 -0500

    Initial Commit

---
+++ TipCalculatorNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculatorNew
+//
+//  Created by Kerry Toonen on 2016-01-22.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculatorNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculatorNew
+//
+//  Created by Kerry Toonen on 2016-01-22.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
